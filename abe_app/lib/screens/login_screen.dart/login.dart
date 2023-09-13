2673856e5f19d6c63b5ae9ea120b9984be27fd43
Zohaib <101661680+zabi1647@users.noreply.github.com>
2023-09-13 21:34:48 +0500

    signup changes

--- abe_app/lib/screens/login_screen.dart/login.dart
+++ abe_app/lib/screens/login_screen.dart/login.dart
@@ -12,74 +12,76 @@ class Login extends StatelessWidget {
     double height = MediaQuery.sizeOf(context).height;
     double width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset('assets/login.png'),
-              spacer(height: height * 0.01),
-              Text(
-                "Sign in",
-                style: style(size: 20),
-              ),
-              spacer(height: height * 0.01),
-              SizedBox(
-                width: width * 0.7,
-                child: inputfield(guss: "Email ID"),
-              ),
-              spacer(height: height * 0.01),
-              SizedBox(
-                width: width * 0.7,
-                child: inputfield(guss: "Password"),
-              ),
-              spacer(height: height * 0.01),
-              const Align(
-                alignment: Alignment(0.6, 1),
-                child: Text("Forget Password"),
-              ),
-              spacer(height: height * 0.03),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  width: width * 0.4,
-                  height: height * 0.05,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(width * 0.3),
-                      color: Colors.black),
-                  child: Center(
-                    child: Text(
-                      "Sign in",
-                      style: style(color: Colors.white),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset('assets/login.png'),
+                spacer(height: height * 0.01),
+                Text(
+                  "Sign in",
+                  style: style(size: 20),
+                ),
+                spacer(height: height * 0.01),
+                SizedBox(
+                  width: width * 0.7,
+                  child: inputfield(guss: "Email ID"),
+                ),
+                spacer(height: height * 0.01),
+                SizedBox(
+                  width: width * 0.7,
+                  child: inputfield(guss: "Password"),
+                ),
+                spacer(height: height * 0.01),
+                const Align(
+                  alignment: Alignment(0.6, 1),
+                  child: Text("Forget Password"),
+                ),
+                spacer(height: height * 0.03),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    width: width * 0.4,
+                    height: height * 0.05,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(width * 0.3),
+                        color: Colors.black),
+                    child: Center(
+                      child: Text(
+                        "Sign in",
+                        style: style(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              spacer(height: height * 0.02),
-              Row(
-                children: [
-                  spacer(width: width * 0.275),
-                  Text(
-                    "Dont have an Account?",
-                    style: style(size: 12),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: ((context) => SignUpScreen())));
-                      },
-                      child: const Text("Sign Up"))
-                ],
-              ),
-              spacer(height: height * 0.03),
-              Text(
-                "Sign in with",
-                style: style(color: Colors.grey),
-              ),
-              spacer(height: height * 0.03),
-              Image.asset('assets/apple.png')
-            ],
+                spacer(height: height * 0.02),
+                Row(
+                  children: [
+                    spacer(width: width * 0.275),
+                    Text(
+                      "Dont have an Account?",
+                      style: style(size: 12),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) => SignUpScreen())));
+                        },
+                        child: const Text("Sign Up"))
+                  ],
+                ),
+                spacer(height: height * 0.03),
+                Text(
+                  "Sign in with",
+                  style: style(color: Colors.grey),
+                ),
+                spacer(height: height * 0.03),
+                Image.asset('assets/apple.png')
+              ],
+            ),
           ),
         ),
       ),
--- abe_app/lib/screens/login_screen.dart/sign_up.dart
+++ abe_app/lib/screens/login_screen.dart/sign_up.dart
@@ -12,99 +12,101 @@ class SignUpScreen extends StatelessWidget {
     double height = MediaQuery.sizeOf(context).height;
     double width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          children: [
-            Image.asset('assets/signup.png'),
-            spacer(height: height * 0.02),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Connect and form business relationships, share innovative ideas, seek potential business partners, sponsors or investors    and share your moments",
-                maxLines: 5,
-                style: TextStyle(
-                  fontSize: 12,
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/signup.png'),
+              spacer(height: height * 0.02),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Connect and form business relationships, share innovative ideas, seek potential business partners, sponsors or investors    and share your moments",
+                  maxLines: 5,
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            spacer(height: height * 0.02),
-            Text(
-              "Sign Up",
-              style: style(
-                size: 20,
+              spacer(height: height * 0.02),
+              Text(
+                "Sign Up",
+                style: style(
+                  size: 20,
+                ),
               ),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(
-                guss: "First name",
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(
+                  guss: "First name",
+                ),
               ),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(
-                guss: "Last name",
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(
+                  guss: "Last name",
+                ),
               ),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(
-                guss: "Number",
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(
+                  guss: "Number",
+                ),
               ),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(
-                guss: "Email",
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(
+                  guss: "Email",
+                ),
               ),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(guss: "Password", showtext: true),
-            ),
-            customizeContainer(
-              width: width * 0.7,
-              input: inputfield(guss: "Confirm Password", showtext: true),
-            ),
-            spacer(height: height * 0.02),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: width * 0.3,
-                height: height * 0.03,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(width * 0.3),
-                    color: Colors.black),
-                child: Center(
-                  child: Text(
-                    "Sign in",
-                    style: style(color: Colors.white),
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(guss: "Password", showtext: true),
+              ),
+              customizeContainer(
+                width: width * 0.7,
+                input: inputfield(guss: "Confirm Password", showtext: true),
+              ),
+              spacer(height: height * 0.02),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: width * 0.3,
+                  height: height * 0.03,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(width * 0.3),
+                      color: Colors.black),
+                  child: Center(
+                    child: Text(
+                      "Sign in",
+                      style: style(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                spacer(width: width * 0.275),
-                Text(
-                  "Already have an Account?",
-                  style: style(size: 12),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => SignUpScreen())));
-                    },
-                    child: const Text("Sign in"))
-              ],
-            ),
-          ],
-        ),
-      )),
+              Row(
+                children: [
+                  spacer(width: width * 0.275),
+                  Text(
+                    "Already have an Account?",
+                    style: style(size: 12),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => SignUpScreen())));
+                      },
+                      child: const Text("Sign in"))
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
